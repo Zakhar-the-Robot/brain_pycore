e72322d65758927b52e66b87a76d1dbbd053fbcb
Andrei Gramakov <mail@agramakov.me>
2020-12-31 20:33:24 +0100

    Update logic of i2c + huge refactoring (breaking)

--- i2c/low_level_i2c.py
+++ i2c/low_level_i2c.py
@@ -1,14 +1,21 @@
-import platform
-from smbus2 import SMBus
+# *************************************************************************
+#
+# Copyright (c) 2020 Andrei Gramakov. All rights reserved.
+#
+# This file is licensed under the terms of the MIT license.
+# For a copy, see: https://opensource.org/licenses/MIT
+#
+# site:    https://agramakov.me
+# e-mail:  mail@agramakov.me
+#
+# *************************************************************************
 
-import logging
+from smbus2 import SMBus
 from threading import Lock
-from time import sleep
+
 bus = SMBus(1)  # indicates /dev/ic2-1
 i2c_mutex = Lock()
 
-# CONFIG_LOG_LEVEL = logging.DEBUG
-CONFIG_LOG_LEVEL = logging.INFO
 
 def i2c_read_byte(addr):
     global i2c_mutex
@@ -17,11 +24,13 @@ def i2c_read_byte(addr):
         print('Read byte %s from %s' % (hex(b), hex(addr)))
         return b
 
+
 def i2c_write_byte(addr, val):
     global i2c_mutex
     with i2c_mutex:
         bus.write_byte(addr, val)
-        print('Wrote byte %s to %s' % (hex(val),hex(addr)))
+        print('Wrote byte %s to %s' % (hex(val), hex(addr)))
+
 
 def i2c_write_byte_data(addr, reg, val):
     global i2c_mutex
@@ -29,20 +38,13 @@ def i2c_write_byte_data(addr, reg, val):
         bus.write_byte_data(addr, reg, val)
         print('Wrote byte %s from %s:%s' % (hex(val), hex(addr), hex(reg)))
 
+
 def i2c_read_byte_from(addr, reg):
     global i2c_mutex
     with i2c_mutex:
-        bus.write_byte_data(addr, reg, 0xFF)
-        b = bus.read_byte(addr)
+        b = bus.read_byte_data(addr, reg)
         return b
 
-def send_cmd(addr, cmd, arg=0x0):
-    if arg:
-        i2c_write_byte_data(addr, 0x1, arg)# write
-    i2c_write_byte_data(addr, 0, cmd)# write
-    while(i2c_read_byte_from(addr,0x0)):
-        sleep(.1)
-    sleep(.1)
 
 def i2cdetect(bus_num):
     b = SMBus(bus_num)
